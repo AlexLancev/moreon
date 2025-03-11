6e650d0a1cda5044c925032ba869aef081b68925
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2025-03-11 20:24:12 +0500

    компонент types_services

--- src/components/types_services/index.tsx
+++ src/components/types_services/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Navigation, Pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 import { SwiperSlide, Swiper } from "swiper/react";
 
 const data = [
@@ -27,35 +27,15 @@ const data = [
       "Популярные групповые тренировки мировых брендов проводят сертифицированные тренеры. Разнообразие программ позволяет каждому найти направление на свой вкус, выбери свою продуктивную тренировку!",
     to_link: "/uslugi/gruppovye-programmy/",
   },
-
-  {
-    head: "СПА и Термы",
-    description:
-      "25 бань и саун, более 70 спа-услуг, джакузи, соляная пещера, двухуровневый бассейн, массаж, контрастные ванны. Панорамные окна и веранда. Всё для полного расслабления после отличных тренировок",
-    to_link: "/uslugi/bani-i-bassejny/",
-  },
-  {
-    head: "Единоборства",
-    description:
-      "Более 500 м² для вашей уверенности в себе! Зона оборудована в соответствии с требованиям профессионального спорта: ринг, октагон, клетка BJJ, настенные боксерские подушки для отработки прямых ударов, подвесные снаряды, татами для борьбы.",
-    to_link: "/uslugi/edinoborstva/",
-  },
-  {
-    head: "Групповые занятия",
-    description:
-      "Популярные групповые тренировки мировых брендов проводят сертифицированные тренеры. Разнообразие программ позволяет каждому найти направление на свой вкус, выбери свою продуктивную тренировку!",
-    to_link: "/uslugi/gruppovye-programmy/",
-  },
 ];
 
 export const Types_services = () => {
   return (
     <div className="types_services">
       <Swiper
-        modules={[Navigation, Pagination]}
+        modules={[Pagination]}
         spaceBetween={20}
         slidesPerView={3}
-        navigation
         pagination={{ clickable: true }}
       >
         {data?.map(({ head, description, to_link }, idx: number) => (
