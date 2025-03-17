5f983766fe54c799cf33a2559a1127c2d31cc9da
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2025-03-17 21:34:28 +0500

    компонент video_reviews

--- src/components/video_reviews_list/index.tsx
+++ src/components/video_reviews_list/index.tsx
@@ -9,8 +9,8 @@ const Video_reviews_data = [
       "На изображении представлен Дмитрий Яшанькин, одетый в черную майку и оранжевые шорты. Он стоит в помещении с большими окнами и пальмами на заднем плане. Дмитрий улыбается и жестикулирует руками. На его запястье виден браслет и часы. Фон оформлен в светлых тонах с элементами декора.",
     person_about_club: "Смотреть видео отзыв от Дмитрия Яшанькина",
     preview_image: {
-      jpg: "/images/video_reviews/review_1.jpg",
-      webp: "/images/video_reviews/review_1.webp",
+      jpg: "/images/video_reviews/result_review_1.jpg",
+      webp: "/images/video_reviews/result_review_1.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456239671&hd=2&autoplay=1",
@@ -20,8 +20,8 @@ const Video_reviews_data = [
       "На изображении представлена Дарья Плужникова, одетая в черный спортивный топ и темные лосины с надписью 'NEBBIA'. Она стоит на балконе фитнес-клуба, держась за перила. На заднем плане видны тренажеры и пальмы. Дарья улыбается и смотрит в камеру. Фон оформлен в светлых тонах с элементами декора.",
     person_about_club: "Смотреть видео отзыв от Дарий Плужниковой",
     preview_image: {
-      jpg: "/images/video_reviews/review_2.jpg",
-      webp: "/images/video_reviews/review_2.webp",
+      jpg: "/images/video_reviews/result_review_2.jpg",
+      webp: "/images/video_reviews/result_review_2.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456240157&hd=2&autoplay=1",
@@ -31,8 +31,8 @@ const Video_reviews_data = [
       "На изображении представлен Дмитрий Яшанькин, который выполняет упражнение тяна в наклоне со штангой в тренажерном зале. Он одет в черную майку и оранжевые шорты, на ногах белые кроссовки. Дмитрий сосредоточен на выполнении упражнения, его руки согнуты под углом, а гантели держатся горизонтально. На заднем плане видны различные тренажеры и оборудование для фитнеса.",
     person_about_club: "Смотреть видео отзыв от Дмиртия Яшанькина",
     preview_image: {
-      jpg: "/images/video_reviews/review_3.jpg",
-      webp: "/images/video_reviews/review_3.webp",
+      jpg: "/images/video_reviews/result_review_3.jpg",
+      webp: "/images/video_reviews/result_review_3.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456239665&hd=2&autoplay=1",
@@ -42,8 +42,8 @@ const Video_reviews_data = [
       "На изображении представлен Сергей Югай, который выполняет упражнение на тренажере в тренажерном зале. Он одет в светло-серую футболку и черные брюки. Сергей сидит на скамье, держась за рукоятки тренажера, его руки вытянуты вверх. На заднем плане видны различные тренажеры и оборудование для фитнеса.",
     person_about_club: "Смотреть видео отзыв от Сергея Югай",
     preview_image: {
-      jpg: "/images/video_reviews/review_4.jpg",
-      webp: "/images/video_reviews/review_4.webp",
+      jpg: "/images/video_reviews/result_review_4.jpg",
+      webp: "/images/video_reviews/result_review_4.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456239681&hd=2&autoplay=1",
@@ -53,8 +53,8 @@ const Video_reviews_data = [
       "На изображении представлен Павел Попов, который стоит в помещении с большими окнами и пальмами на заднем плане. Он одет в светло-серую футболку с надписью 'CARNITY WARRIOR' и черные брюки. На его запястье виден смарт-часы и браслет. Павел улыбается и смотрит в камеру. Фон оформлен в светлых тонах с элементами декора.",
     person_about_club: "Смотреть видео отзыв от Павла Попова",
     preview_image: {
-      jpg: "/images/video_reviews/review_5.jpg",
-      webp: "/images/video_reviews/review_5.webp",
+      jpg: "/images/video_reviews/result_review_5.jpg",
+      webp: "/images/video_reviews/result_review_5.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456240062&hd=2&autoplay=1",
@@ -64,8 +64,8 @@ const Video_reviews_data = [
       "На изображении представлены два мужчины в тренажерном зале. Один из них, одетый в черную майку с черепом и черные шорты, держит световой меч. Второй мужчина, одетый в желтую майку и темно-синие шорты, показывает жест 'лайк'. Оба улыбаются и смотрят в камеру. На заднем плане видны различные тренажеры и оборудование для фитнеса.",
     person_about_club: "Смотреть видео отзыв от Романа Могучий",
     preview_image: {
-      jpg: "/images/video_reviews/review_6.jpg",
-      webp: "/images/video_reviews/review_6.webp",
+      jpg: "/images/video_reviews/result_review_6.jpg",
+      webp: "/images/video_reviews/result_review_6.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456240018&hd=2&autoplay=1",
@@ -75,8 +75,8 @@ const Video_reviews_data = [
       "На изображении представлены два человека в тренажерном зале. Слева стоит молодой человек, одетый в белую футболку и черные шорты, показывая бицепс. Справа стоит Максим Битаров, одетый в черную футболку с принтом и черную кепку, также показывая бицепс. Оба улыбаются и смотрят в камеру. На заднем плане видны различные тренажеры и оборудование для фитнеса.",
     person_about_club: "Смотреть видео отзыв от Максима Битарова",
     preview_image: {
-      jpg: "/images/video_reviews/review_7.jpg",
-      webp: "/images/video_reviews/review_7.webp",
+      jpg: "/images/video_reviews/result_review_7.jpg",
+      webp: "/images/video_reviews/result_review_7.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456239913&hd=2&autoplay=1",
@@ -86,8 +86,8 @@ const Video_reviews_data = [
       "На изображении представлен Гога Тупурия, который стоит в помещении с большими окнами и пальмами на заднем плане. Он одет в черную майку и желтые шорты. Гога улыбается и жестикулирует руками. На его запястье виден браслет. Фон оформлен в светлых тонах с элементами декора.",
     person_about_club: "Смотреть видео отзыв от Гога Топурия",
     preview_image: {
-      jpg: "/images/video_reviews/review_8.jpg",
-      webp: "/images/video_reviews/review_8.webp",
+      jpg: "/images/video_reviews/result_review_8.jpg",
+      webp: "/images/video_reviews/result_review_8.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456239856&hd=2&autoplay=1",
@@ -97,8 +97,8 @@ const Video_reviews_data = [
       "На изображении представлен Владимир Московченко, который стоит в тренажерном зале. Он одет в ярко-желтую майку с надписью 'KIPSTA' и черные шорты. У Владимира борода и татуировки на руках. На его запястье виден смарт-часы. Владимир жестикулирует руками и смотрит в камеру. На заднем плане видны различные тренажеры и оборудование для фитнеса.",
     person_about_club: "Смотреть видео отзыв от Владимира Московченко",
     preview_image: {
-      jpg: "/images/video_reviews/review_9.jpg",
-      webp: "/images/video_reviews/review_9.webp",
+      jpg: "/images/video_reviews/result_review_9.jpg",
+      webp: "/images/video_reviews/result_review_9.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456239713&hd=2&autoplay=1",
@@ -108,8 +108,8 @@ const Video_reviews_data = [
       "На изображении представлен Александр Фёдоров, который стоит в помещении с большими окнами и пальмами на заднем плане. Он одет в белую футболку с логотипом 'AF-Club'. Александр жестикулирует руками и смотрит в камеру. На его запястье виден браслет. Фон оформлен в светлых тонах с элементами декора.",
     person_about_club: "Смотреть видео отзыв от Александра Фёдорова",
     preview_image: {
-      jpg: "/images/video_reviews/review_10.jpg",
-      webp: "/images/video_reviews/review_10.webp",
+      jpg: "/images/video_reviews/result_review_10.jpg",
+      webp: "/images/video_reviews/result_review_10.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456239701&hd=2&autoplay=1",
@@ -119,8 +119,8 @@ const Video_reviews_data = [
       "На изображении представлен Станислав Линдовер, который стоит в помещении с большими окнами и пальмами на заднем плане. Он одет в зеленую рубашку-поло и темные джинсы. Станислав жестикулирует рукой и смотрит в камеру. На его запястье виден браслет. Фон оформлен в светлых тонах с элементами декора.",
     person_about_club: "Смотреть видео отзыв от Станислава Линдовер",
     preview_image: {
-      jpg: "/images/video_reviews/review_11.jpg",
-      webp: "/images/video_reviews/review_11.webp",
+      jpg: "/images/video_reviews/result_review_11.jpg",
+      webp: "/images/video_reviews/result_review_11.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456239664&hd=2&autoplay=1",
@@ -130,8 +130,8 @@ const Video_reviews_data = [
       "На изображении представлен Дмитрий Голубочкин, который стоит в помещении с большими окнами и пальмами на заднем плане. Он одет в черную футболку с надписью 'NO GENETICS NO GAIN' и черную кепку с логотипом 'GORILLA WEAR'. У Дмитрия борода, и он улыбается, смотря в камеру. На его запястье виден браслет. Фон оформлен в светлых тонах с элементами декора.",
     person_about_club: "Смотреть видео отзыв от Дмитрия Голубочкина",
     preview_image: {
-      jpg: "/images/video_reviews/review_12.jpg",
-      webp: "/images/video_reviews/review_12.webp",
+      jpg: "/images/video_reviews/result_review_12.jpg",
+      webp: "/images/video_reviews/result_review_12.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456240243&hd=2&autoplay=1",
@@ -141,8 +141,8 @@ const Video_reviews_data = [
       "На изображении представлен Виктор Симкин, который стоит в помещении с большими окнами и пальмами на заднем плане. Он одет в черную майку и черные шорты, на ногах белые кроссовки. Виктор показывает жест 'лайк' и улыбается, смотря в камеру. На его запястье виден браслет. Фон оформлен в светлых тонах с элементами декора.",
     person_about_club: "Смотреть видео отзыв от Виктора Симкина",
     preview_image: {
-      jpg: "/images/video_reviews/review_13.jpg",
-      webp: "/images/video_reviews/review_13.webp",
+      jpg: "/images/video_reviews/result_review_13.jpg",
+      webp: "/images/video_reviews/result_review_13.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456240220&hd=2&autoplay=1",
@@ -152,8 +152,8 @@ const Video_reviews_data = [
       "На изображении представлен Денис Цыпленков, который стоит в помещении с большими окнами и пальмами на заднем плане. Он одет в серую толстовку с принтом черепа и черные штаны с надписью 'Inferno'. На голове у Дениса черная кепка. Он смотрит в сторону и держится за перила. Фон оформлен в светлых тонах с элементами декора.",
     person_about_club: "Смотреть видео отзыв от Дениса Цыпленкова",
     preview_image: {
-      jpg: "/images/video_reviews/review_14.jpg",
-      webp: "/images/video_reviews/review_14.webp",
+      jpg: "/images/video_reviews/result_review_14.jpg",
+      webp: "/images/video_reviews/result_review_14.webp",
     },
     link_to_video:
       "https://vk.com/video_ext.php?oid=-72055975&id=456240222&hd=2&autoplay=1",
@@ -167,7 +167,7 @@ export const Video_reviews_list = () => {
     <Swiper
       modules={[Pagination]}
       spaceBetween={20}
-      slidesPerView={3}
+      slidesPerView={4}
       pagination={{ clickable: true }}
     >
       {Video_reviews_data?.map(
@@ -184,15 +184,17 @@ export const Video_reviews_list = () => {
             <button
               type="button"
               className="btn_review"
+              style={{display: `${isVideoActive === idx && 'none'}`}}
               onClick={() =>
                 setIsVideoActive(isVideoActive === idx ? null : idx)
               }
             >
               <picture>
-                <source srcSet={jpg} type="image/webp" />
+                <source srcSet={webp} type="image/webp" />
                 <img
-                  className="block w-full"
-                  width={370}
+                  className="block w-full duration-700 hover:scale-[1.1]"
+                  width={320}
+                  height={570}
                   src={jpg}
                   alt={description}
                   aria-label={description}
@@ -204,6 +206,9 @@ export const Video_reviews_list = () => {
             {isVideoActive === idx && (
               <iframe
                 src={link_to_video}
+                className="rounded-2xl overflow-hidden"
+                width={320}
+                height={570}
                 allow="autoplay; encrypted-media; fullscreen; picture-in-picture;"
                 allowFullScreen
               ></iframe>
