52c1d8798a29e05e00d54bf534eeffbc3395c5ff
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2025-06-12 20:16:27 +0500

    компонент range_slider

--- src/components/range_slider/index.tsx
+++ src/components/range_slider/index.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
+
+import classNames from "classnames";
+
 import { Slider } from "../ui/slider";
 import { Input } from "../ui/input";
 
 type Range_slider_props_type = {
-  onMonthsChange: (months: number) => void; // Callback для передачи количества месяцев
+  onMonthsChange: (months: number) => void;
+  className?: string;
 };
 
-export const Range_slider = ({ onMonthsChange }: Range_slider_props_type) => {
-  const [sliderValue, setSliderValue] = useState<number[]>([1]);
-  const [inputValue, setInputValue] = useState<string>("1");
+export const Range_slider = ({
+  onMonthsChange,
+  className,
+}: Range_slider_props_type) => {
+  const [sliderValue, setSliderValue] = useState<number[]>([3]);
+  const [inputValue, setInputValue] = useState<string>("3");
 
   const renderMonth = (idx: number) => {
     if (idx > 1 && idx < 5) return "месяца";
@@ -37,25 +44,27 @@ export const Range_slider = ({ onMonthsChange }: Range_slider_props_type) => {
 
   const [idx] = sliderValue;
   return (
-    <>
-      <div className="flex items-center gap-x-3">
+    <div className={classNames("", className)}>
+      <strong className="text-xl block mb-5">Количество месяцев</strong>
+      <div className="flex items-center gap-x-5 max-w-[600px]">
         <Input
-          type="number"
-          placeholder="1"
-          min={1}
-          max={12}
+          className="max-w-[50px] text-center"
+          type="text"
+          pattern="\d*"
+          maxLength={2}
+          placeholder="3"
           value={inputValue}
           onChange={handleInputChange}
         />
 
         <Slider
+          className="w-[240px] cursor-pointer py-3"
           defaultValue={[1]}
           min={1}
           max={12}
           step={1}
           value={sliderValue}
           onValueChange={handleSliderChange}
-          className="w-[240px]"
         />
 
         <div className="flex items-center gap-x-1">
@@ -63,6 +72,6 @@ export const Range_slider = ({ onMonthsChange }: Range_slider_props_type) => {
           <span>{renderMonth(idx)}</span>
         </div>
       </div>
-    </>
+    </div>
   );
 };
