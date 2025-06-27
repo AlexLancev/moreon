f93bdb640a4ec9279b26e411ec1e6664cebaee03
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2025-06-27 22:26:31 +0500

    адаптив для opening_hours

--- src/components/opening_hours/index.tsx
+++ src/components/opening_hours/index.tsx
@@ -1,16 +1,18 @@
 export const Opening_hours = () => {
   return (
     <div>
-      <strong className="block mb-5">Время работы:</strong>
-      <ul className="mb-3">
-        <li className="mb-1">Будни: 07:00 — 24:00</li>
+      <strong className="block mb-5 lg:text-base xl:text-xl 2xl:text-2xl 3xl:text-3xl">
+        Время работы:
+      </strong>
+      <ul className="mb-3 *:lg:text-base *:xl:text-xl *:2xl:text-2xl *:3xl:text-3xl">
+        <li className="mb-1 2xl:mb-4">Будни: 07:00 — 24:00</li>
         <li>Бассейн: 07:00 — 23:45</li>
       </ul>
-      <ul className="mb-3">
-        <li className="mb-1">Выходные: 09:00 — 24:00</li>
+      <ul className="mb-3 *:lg:text-base *:xl:text-xl *:2xl:text-2xl *:3xl:text-3xl">
+        <li className="mb-1 2xl:mb-4">Выходные: 09:00 — 24:00</li>
         <li>Бассейн: 09:00 — 23:45</li>
       </ul>
-      <ul>
+      <ul className="*:lg:text-base *:xl:text-xl *:2xl:text-2xl *:3xl:text-3xl">
         <li>Отдел продаж: 10:00 — 22:00</li>
       </ul>
     </div>
