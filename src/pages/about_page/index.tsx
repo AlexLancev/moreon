cfae53b1703bef147058eea686064618aee92183
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2025-07-12 19:49:35 +0500

    адаптив для about_page

--- src/pages/about_page/index.tsx
+++ src/pages/about_page/index.tsx
@@ -34,11 +34,11 @@ const About_page = () => {
         tabs_store={
           get_tabs_store("component_water_zone") as Water_zone_tabs_type
         }
-      /> */}
-      <About_questions />
-      {/* <Profitable_visits />
+      />
+      <About_questions /> */}
+      <Profitable_visits />
       <Every_card />
-      <About_reviews /> */}
+      <About_reviews />
     </>
   );
 };
