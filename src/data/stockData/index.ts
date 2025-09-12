3340147588e2b529e871ccaa99cc4bee7e3637c9
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2025-09-12 19:46:05 +0500

    убрал лишние проверки stockData

--- src/data/stockData/index.ts
+++ src/data/stockData/index.ts
@@ -1,21 +1,6 @@
 import supabase from "../supabase";
 
-export const stockData = async (): Promise<StockType[]> => {
-  try {
-    const { data, error } = await supabase.from("stock").select("*");
-
-    if (error) {
-      console.error("Ошибка при получении данных:", error);
-      throw new Error("Не удалось получить данные");
-    }
-
-    if (!data || data.length === 0) {
-      throw new Error("Пустой ответ от сервера");
-    }
-
-    return data;
-  } catch (err) {
-    console.error("Произошла ошибка:", err);
-    throw err;
-  }
+export const stockData = async (): Promise<StockType[] | null> => {
+  const { data } = await supabase.from("stock").select("*");
+  return data;
 };
