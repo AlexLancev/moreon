de2fa611ec9423642f00eb7a46232ec4c9cca87a
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2025-09-18 22:42:36 +0500

    fix aboutQuestionsData

--- src/data/aboutQuestionsData/index.tsx
+++ src/data/aboutQuestionsData/index.tsx
@@ -4,7 +4,6 @@ export const aboutQuestionsData = async (): Promise<AboutQuestionType[]> => {
   const { data, error } = await supabase.from("aboutQuestions").select("*");
 
   if (error) {
-    console.error("Ошибка при получении данных:", error);
     throw new Error("Не удалось получить данные");
   }
 
