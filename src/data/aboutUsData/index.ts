8e1817ab5b8cf903dc7e0792ad961202e0a59d2d
Alex Vavulo <alekseivavulo9@gmail.com>
2025-09-20 20:54:02 +0500

    fix aboutUsData

--- src/data/aboutUsData/index.ts
+++ src/data/aboutUsData/index.ts
@@ -1,21 +1,11 @@
 import supabase from "../supabase";
 
-export const aboutUsData = async (): Promise<AboutUsType[]> => {
-  try {
-    const { data, error } = await supabase.from("aboutUs").select("*");
+export const aboutUsData = async (): Promise<AboutUsType[] | null> => {
+  const { data, error } = await supabase.from("aboutUs").select("*");
 
-    if (error) {
-      console.error("Ошибка при получении данных:", error);
-      throw new Error("Не удалось получить данные");
-    }
-
-    if (!data || data.length === 0) {
-      throw new Error("Пустой ответ от сервера");
-    }
-
-    return data;
-  } catch (err) {
-    console.error("Произошла ошибка:", err);
-    throw err;
+  if (error) {
+    throw new Error("Не удалось получить данные");
   }
+
+  return data;
 };
